4e44c8ef74040f642a21a1a2d332a371ba6ba6fa
dimkary <43819376+dimkary@users.noreply.github.com>
2022-12-02 12:04:44 +0100

    Update AoC22_1.py

--- Aoc 2022/1/AoC22_1.py
+++ Aoc 2022/1/AoC22_1.py
@@ -1,7 +1,5 @@
 with open("./input.txt") as f:
     a = f.read().split("\n\n")
-max_cal = 0
-max_ind = len(a) + 1
 dic = {}
 for i in (range(len(a))):
     dic[i] = sum([int(x) for x in a[i].split()])
